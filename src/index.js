eef0b3f009733574cabda77985589649029f44ab
Leonid Logvinov <logvinov.leon@gmail.com>
2017-04-05 18:52:09 +0200

    Fix linter warning

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ async function main() {
   app.use(schemaValidationErrorMiddleware);
 
   app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
     console.log('Example app listening on port 3000!');
   });
 }
