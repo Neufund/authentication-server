b30e40423b81c16270a0f1effdcdfce3156d8cdb
Leonid Logvinov <logvinov.leon@gmail.com>
2017-04-06 12:53:47 +0200

    Add RECAPTCHA and async error handling

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,14 @@ const validate = require('express-jsonschema').validate;
 const speakeasy = require('speakeasy');
 const db = require('sqlite');
 const fs = require('fs');
+const Recaptcha = require('recaptcha-verify');
+const { toPromise, catchAsyncErrors } = require('../utils');
 const statements = require('../db');
+
+const recaptcha = new Recaptcha({
+  secret: process.env.RECAPTCHA_SECRET_KEY,
+  verbose: true,
+});
 
 const userCreateSchema = JSON.parse(fs.readFileSync('./schemas/userCreateSchema.json'));
 
@@ -12,17 +19,17 @@ router.get('/', async (req, res) => {
   res.send(users);
 });
 
-router.post('/', validate({ body: userCreateSchema }), async (req, res, next) => {
-  try {
-    const reqParams = (await statements).dollarPrefixKeys(req.body);
-    const timeBasedOneTimeSecret = speakeasy.generateSecret({ length: 20 });
-    const $timeBasedOneTimeSecret = timeBasedOneTimeSecret.base32;
-    const queryParams = Object.assign({}, reqParams, { $timeBasedOneTimeSecret });
-    await statements.userInsertStmt.run(queryParams);
-    res.send(queryParams);
-  } catch (e) {
-    next(e);
+router.post('/', validate({ body: userCreateSchema }), catchAsyncErrors(async (req, res) => {
+  const recaptchaResponse = await toPromise(recaptcha.checkResponse.bind(recaptcha))(req.body['g-recaptcha-response']);
+  if (!recaptchaResponse.success) {
+    throw new Error(recaptchaResponse['error-codes']);
   }
-});
+  const reqParams = (await statements).dollarPrefixKeys(req.body);
+  const timeBasedOneTimeSecret = speakeasy.generateSecret({ length: 20 });
+  const $timeBasedOneTimeSecret = timeBasedOneTimeSecret.base32;
+  const queryParams = Object.assign({}, reqParams, { $timeBasedOneTimeSecret });
+  await statements.userInsertStmt.run(queryParams);
+  res.send(queryParams);
+}));
 
 module.exports = router;
