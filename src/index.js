016e8465f7dd476b3f48fc39876e293025c88786
Leonid Logvinov <logvinov.leon@gmail.com>
2017-04-06 16:40:36 +0200

    Add corss middleware

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const db = require('sqlite');
 const statements = require('./db');
 const router = require('./router');
 const bodyParser = require('body-parser');
+const corsMiddleware = require('./middlewares/corsMiddleware');
 const schemaValidationErrorMiddleware = require('./middlewares/schemaValidationErrorMiddleware');
 
 const app = express();
@@ -15,6 +16,7 @@ async function main() {
   await statements.init();
 
   app.use(bodyParser.json());
+  app.use(corsMiddleware);
   app.use(router);
   app.use(schemaValidationErrorMiddleware);
 
