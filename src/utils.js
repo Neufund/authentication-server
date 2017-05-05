55ca7f65a649489b459a6f790265f3354b3340de
Remco Bloemen <remco-git@xn--2-umb.com>
2017-05-05 17:47:24 +0100

    fix: arrow functions dont have arguments object

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,15 @@
-const callback = (accept, reject) => (error, data) => {
-  if (arguments.length === 0) {
-    accept();
-  } else if (error) {
-    reject(error);
-  } else {
-    accept(data);
-  }
-};
+const callback = (accept, reject) =>
+  function (error, data) {
+    if (arguments.length === 0) {
+      accept();
+    } else if (error) {
+      reject(error);
+    } else {
+      accept(data);
+    }
+  };
 
-exports.toPromise = func =>
-  (...args) =>
-    new Promise((accept, reject) =>
-      func(...args, callback(accept, reject)));
+exports.toPromise = func => (...args) =>
+  new Promise((accept, reject) => func(...args, callback(accept, reject)));
 
 exports.catchAsyncErrors = asyncFunction => (...args) => asyncFunction(...args).catch(args[2]);
